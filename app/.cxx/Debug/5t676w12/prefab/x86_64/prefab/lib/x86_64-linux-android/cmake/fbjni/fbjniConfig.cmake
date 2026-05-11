ce9f383b02c9445b6be9622f5880fc4fec178578
shrikant <shrikant.ingole@du.ae>
2026-05-12 01:02:09 +0530

    ignire

--- app/.cxx/Debug/5t676w12/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/shrikant.d.ingole/.gradle/caches/8.14.4/transforms/9971b78e026cf4a8664db114a6f5805b/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/shrikant.d.ingole/.gradle/caches/8.14.4/transforms/9971b78e026cf4a8664db114a6f5805b/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- app/.cxx/Debug/5t676w12/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::hermesvm)
-add_library(hermes-engine::hermesvm SHARED IMPORTED)
-set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "/Users/shrikant.d.ingole/.gradle/caches/8.14.4/transforms/28a3e321c8e2bc15e5936d3bebc27f5a/transformed/hermes-android-250829098.0.10-debug/prefab/modules/hermesvm/libs/android.x86_64/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/shrikant.d.ingole/.gradle/caches/8.14.4/transforms/28a3e321c8e2bc15e5936d3bebc27f5a/transformed/hermes-android-250829098.0.10-debug/prefab/modules/hermesvm/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
